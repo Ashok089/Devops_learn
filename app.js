de64ac3277060dea4fbcdcd73606e95bd60e062f
Ashok089 <148200587+Ashok089@users.noreply.github.com>
2026-02-10 19:10:18 +0530

    Updated app message

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps CI/CD Project Running 🚀");
+  res.send("DevOps CI/CD Updated Version 🔥");
+  res.send("In this we are running whole flow again. 🔥");
 });
 
 app.listen(3000, () => {
   console.log("App running on port 3000");
 });
-
 
