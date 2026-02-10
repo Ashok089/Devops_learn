7583941738473d9ecb9a574487fd7f3bf70dceb5
Ashok089 <148200587+Ashok089@users.noreply.github.com>
2026-02-10 11:05:51 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("DevOps CI/CD Project Running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
+
+
